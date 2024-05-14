016087949848ec081deaba152a6e0f701fe89943
umka9999 <umka9999@yandex.ru>
2024-05-14 17:09:51 +0300

    add few links in base html

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from shop.views import home, profile_view, login_user
+from shop.views import home, profile_view, login_user, about
 from django.contrib.auth.views import LoginView
 
 app_name = 'shop'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', home, name='about'),
     path('profile', profile_view, name='profile'),
     path('login', login_user, name='login'),
 
--- shop/views.py
+++ shop/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, "shop/home.html")
 
+def about(request):
+    return render(request, "shop/about.html")
 
 def login_user(request):
     return render(request, "registration/login.html")
